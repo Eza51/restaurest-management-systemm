081fca8a03997774cb53e35041d562f4f9432bee
Eza51 <nowshinnjerineza51@gmail.com>
2024-11-29 01:31:27 +0600

    second commit

---
+++ resources/views/front-end/home.blade.php
@@ -0,0 +1,9 @@
+@extends('front-end.layouts.master')
+
+@section('content')
+    @include('front-end.nav')
+    @include('front-end.service')
+   @include('front-end.about')
+   @include('front-end.menu')
+   
+@endsection
